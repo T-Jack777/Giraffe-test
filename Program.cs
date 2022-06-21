a8e6bdf05a669e462c3c245f84da83d678b9c4bd
Antec Razer <Antec Razer@DESKTOP-3UBCGQE>
2022-06-21 17:24:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Giraffe_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+
+
+
+            Console.ReadLine();
+
+
+
+
+                
+        }
+    }
+}
